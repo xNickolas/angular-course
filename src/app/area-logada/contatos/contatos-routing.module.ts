ba1517940de557c9ae77065bd151acf5211c6b29
xNickolas <nickolsctt@gmail.com>
2021-02-23 12:03:09 -0300

    feat: edit contact

--- src/app/area-logada/contatos/contatos-routing.module.ts
+++ src/app/area-logada/contatos/contatos-routing.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DetalharContatoComponent } from './detalhar-contato/detalhar-contato.component';
+import { EditarContatoComponent } from './editar-contato/editar-contato.component';
 import { ListarContatosComponent } from './listar-contatos/listar-contatos.component';
-import { NovoContatoComponent } from './novo-contato/novo-contato.component';
 
 
 const routes: Routes = [{
   path: '', component: ListarContatosComponent,
 }, {
-  path: 'novo', component: NovoContatoComponent,
+  path: 'novo', component: EditarContatoComponent,
 }, {
   path: ':id', component: DetalharContatoComponent,
+}, {
+  path: ':id/editar', component: EditarContatoComponent,
 }];
 
 @NgModule({

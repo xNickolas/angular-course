e98731ea87d036fe172885f94eaf5d3040832e2d
xNickolas <nickolsctt@gmail.com>
2021-02-23 13:58:07 -0300

    feat: working w/ grouped forms

--- src/app/area-logada/contatos/editar-contato/editar-contato.component.ts
+++ src/app/area-logada/contatos/editar-contato/editar-contato.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { finalize, take } from 'rxjs/operators';
@@ -42,9 +42,11 @@ export class EditarContatoComponent implements OnInit {
     this.contatoForm = this.formBuilder.group({
       nome: ['', Validators.required],
       cpf: ['', Validators.required],
+      // dadosBancarios: this.formBuilder.group({
       ag: ['', [Validators.required, Validators.minLength(4)]],
       cc: ['', [Validators.required, Validators.minLength(5)]],
       banco: ['', Validators.required],
+      // }),
     });
   }
 
@@ -73,16 +75,20 @@ export class EditarContatoComponent implements OnInit {
     console.error(error);
   }
 
-  validateAllFormFields() {
-    Object.keys(this.contatoForm.controls).forEach(field => {
-      const control = this.contatoForm.get(field);
-      control.markAsTouched();
+  validarCamposDoFormulario(form: FormGroup) {
+    Object.keys(form.controls).forEach(field => {
+      const control = form.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched();
+      } else if (control instanceof FormGroup) {
+        this.validarCamposDoFormulario(control);
+      }
     });
   }
 
   onSubmit() {
     if (this.contatoForm.invalid) {
-      this.validateAllFormFields();
+      this.validarCamposDoFormulario(this.contatoForm);
       return;
     }
 
